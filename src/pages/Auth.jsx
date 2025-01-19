b6a01c4bc7c5955e5eb9b2d0eda6989651668594
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2025-01-19 12:33:36 +0530

    minor fix

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -3,8 +3,19 @@ function Auth({ reqType }) {
     <div className="flex flex-grow p-6">
       <div className="flex flex-col flex-grow gap-6 p-6 border rounded-lg">
         <h1 className="text-3xl text-center">{reqType}</h1>
-        <span className="flex flex-col justify-center gap-6">
-          <label className="flex items-center gap-2 input input-bordered">
+        <label className="flex items-center gap-2 input input-bordered">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+          </svg>
+          <input type="text" className="grow" placeholder="Username" />
+        </label>
+        <span className="flex justify-center gap-6 max-sm:flex-col">
+          <label className="flex items-center gap-2 sm:w-1/2 input input-bordered">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
@@ -14,9 +25,15 @@ function Auth({ reqType }) {
               <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
               <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
             </svg>
-            <input type="text" id="email" name="email" className="grow" placeholder="Email" />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="grow"
+              placeholder="Email"
+            />
           </label>
-          <label className="flex items-center gap-2 input input-bordered">
+          <label className="flex items-center gap-2 sm:w-1/2 input input-bordered">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
@@ -29,63 +46,68 @@ function Auth({ reqType }) {
                 clipRule="evenodd"
               />
             </svg>
-            <input type="password" id="password" name="password" className="grow" value="password" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="grow"
+            />
           </label>
         </span>
         {reqType === "Sign Up" ? (
-          <div className="flex gap-6">
+          <div className="flex gap-6 max-sm:flex-col">
             <p>Account Type:</p>
-            <div class="flex items-center">
+            <div className="flex items-center">
               <input
                 defaultChecked
                 id="student"
                 type="radio"
                 name="default-radio"
-                class="radio"
+                className="radio"
               />
               <label
-                for="student"
-                class="ms-2 text-sm font-medium text-gray-900"
+                htmlFor="student"
+                className="text-sm font-medium text-gray-900 ms-2"
               >
                 Student
               </label>
             </div>
-            <div class="flex items-center">
+            <div className="flex items-center">
               <input
                 id="teacher"
                 type="radio"
                 name="default-radio"
-                class="radio"
+                className="radio"
               />
               <label
-                for="teacher"
-                class="ms-2 text-sm font-medium text-gray-900"
+                htmlFor="teacher"
+                className="text-sm font-medium text-gray-900 ms-2"
               >
                 Teacher
               </label>
             </div>
-            <div class="flex items-center">
+            <div className="flex items-center">
               <input
                 id="parent"
                 type="radio"
                 name="default-radio"
-                class="radio"
+                className="radio"
               />
               <label
-                for="parent"
-                class="ms-2 text-sm font-medium text-gray-900"
+                htmlFor="parent"
+                className="text-sm font-medium text-gray-900 ms-2"
               >
                 Parent
               </label>
             </div>
-            <div class="flex items-center">
+            <div className="flex items-center">
               <input
                 id="admin"
                 type="radio"
                 name="default-radio"
-                class="radio"
+                className="radio"
               />
-              <label for="admin" class="ms-2 text-sm font-medium text-gray-900">
+              <label htmlFor="admin" className="text-sm font-medium text-gray-900 ms-2">
                 Admin
               </label>
             </div>
