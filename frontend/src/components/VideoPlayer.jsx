73fd2ff5a8aeb4c9b79d13d22dd5e521bb5b69bd
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2025-02-01 19:15:43 +0530

    minor fix

--- frontend/src/components/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer.jsx
@@ -4,15 +4,15 @@ const VideoPlayer = () => {
   const { myVideo, userVideo } = useContext(SocketContext);
   return (
     <div className="flex gap-6 max-md:flex-col">
-      <div className="relative w-full">
-        <video playsInline ref={userVideo} autoPlay className="w-full" />
+      <div className="relative w-full bg-black/20">
+        <video playsInline ref={userVideo} autoPlay className="w-full aspect-video" />
         <div className="w-[100px] max-md:w-[50px] max-sm:w-8 absolute right-0 bottom-0 bg-black/50">
           <video
             playsInline
             muted
             ref={myVideo}
             autoPlay
-            className="w-full"
+            className="w-full aspect-video"
           />
         </div>
       </div>
