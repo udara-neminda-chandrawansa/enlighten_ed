79201afda69fc14657fe7093cf26bdf18fd9dd54
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2025-02-01 21:48:46 +0530

    logout done

--- frontend/src/pages/Dash.jsx
+++ frontend/src/pages/Dash.jsx
@@ -18,7 +18,7 @@ import Options from "../components/Options";
 import Notifications from "../components/Notifications";
 import UpdateAccount from "../components/UpdateAccount";
 
-function Dashboard(){
+function Dashboard() {
   const [activeSpace, setActiveSpace] = useState("");
   const [isSidebarOpen, setSidebarOpen] = useState(false);
   const [location, navigate] = useLocation();
@@ -107,7 +107,32 @@ function Dashboard(){
         return (
           <div className="p-6">
             <h2 className="mb-4 text-2xl font-bold">Account Management</h2>
-            <UpdateAccount/>
+            <UpdateAccount />
+            <div className="mt-6">
+              <button
+                className="flex items-center gap-3 p-2 text-white bg-red-500 rounded-md"
+                onClick={() => handleLogout()}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-box-arrow-left"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"
+                  />
+                </svg>
+                Logout
+              </button>
+            </div>
           </div>
         );
       default:
@@ -211,6 +236,6 @@ function Dashboard(){
       );
     }
   }
-};
+}
 
 export default Dashboard;
