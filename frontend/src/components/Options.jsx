e07862f9e1504250df4b7a8e87bb52d4c9aa80cf
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2025-02-02 20:02:24 +0530

    minor fix

--- frontend/src/components/Options.jsx
+++ frontend/src/components/Options.jsx
@@ -48,12 +48,15 @@ const Options = () => {
       const result = await getUsers();
       if (result.success) {
         setUsers(result.users);
-        console.log("Users Loaded!");
+        //console.log("Users Loaded!");
       } else {
         console.log("Message:", result.message);
       }
     };
     fetchUsers();
+  });
+
+  useEffect(() => {
     setName(JSON.parse(Cookies.get("auth"))["username"]);
     updatePeerId(me);
   }, []);
@@ -81,11 +84,11 @@ const Options = () => {
                       width="16"
                       height="16"
                       fill="currentColor"
-                      class="bi bi-telephone-fill"
+                      className="bi bi-telephone-fill"
                       viewBox="0 0 16 16"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
                       />
                     </svg>
@@ -93,26 +96,26 @@ const Options = () => {
                   </button>
                 )}
                 {callAccepted && !callEnded ? (
-            <button
-              className="flex items-center gap-2 px-3 py-1 text-sm text-white bg-red-500 rounded-md"
-              onClick={leaveCall}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-telephone-minus-fill"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zM10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5"
-                />
-              </svg>
-              Hang up
-            </button>
-          ) : null}
+                  <button
+                    className="flex items-center gap-2 px-3 py-1 text-sm text-white bg-red-500 rounded-md"
+                    onClick={leaveCall}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-telephone-minus-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zM10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5"
+                      />
+                    </svg>
+                    Hang up
+                  </button>
+                ) : null}
               </div>
             ))
           ) : (
